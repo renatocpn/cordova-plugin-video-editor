f9eee11dd1dedee44c46386eb97d244feac71f43
digitalalchimia-csm <47528410+digitalalchimia-csm@users.noreply.github.com>
2019-08-02 10:08:16 -0300

    Update VideoEditor.java

--- src/android/VideoEditor.java
+++ src/android/VideoEditor.java
@@ -221,7 +221,7 @@ public class VideoEditor extends CordovaPlugin {
                     }
 
                     MediaTranscoder.getInstance().transcodeVideo(fd, outputFile.getAbsolutePath(),
-                            new CustomAndroidFormatStrategy(videoBitrate, fps, width, height), listener, videoDuration);
+                            new CustomAndroidFormatStrategy(videoBitrate, fps, width, height), listener);
 
                 } catch (Throwable e) {
                     Log.e(TAG, "transcode exception", e);
